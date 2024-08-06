71bc9000e0eab8a5007cfe32f7f4a5fa956c3f38
Crisfertho <cristhofer.rg06@gmail.com>
2024-08-06 15:04:38 -0600

    deployment domcloud

--- Router.php
+++ Router.php
@@ -23,8 +23,8 @@ class Router{
         $rutasprotegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar',
         '/vendedores/crear', 'vendedores/actualizar', '/vendedores/eliminar' ];
 
-        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
-        //$urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        //$urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
         if ($metodo === 'GET') {
